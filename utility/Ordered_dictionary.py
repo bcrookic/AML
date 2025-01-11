57453bf84534e149054b6786587154245b106dbb
bcrookic <95067480+bcrookic@users.noreply.github.com>
2025-01-11 23:08:38 +0800

    Update Ordered_dictionary.py

--- utility/Ordered_dictionary.py
+++ utility/Ordered_dictionary.py
@@ -1,6 +1,6 @@
 import pickle
 
-with open('./dataset/embedding_sim.pkl', 'rb') as file:
+with open('../dataset/embedding_sim.pkl', 'rb') as file:
     data = pickle.load(file)
 
 sorted_keys = sorted(data.keys(), key=lambda x: int(x))
@@ -12,5 +12,5 @@ ordered_dict = OrderedDict()
 for key in sorted_keys:
     ordered_dict[key] = data[key]
 
-with open('./data/reduced_embedding_sim.pkl', 'wb') as file:
+with open('../data/reduced_embedding_sim.pkl', 'wb') as file:
     pickle.dump(ordered_dict, file)
