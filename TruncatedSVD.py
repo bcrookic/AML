afa4e15cc931d5ddbcc3fbc2a030939d2fd490fa
bcrookic <95067480+bcrookic@users.noreply.github.com>
2025-01-11 23:02:25 +0800

    Update TruncatedSVD.py

--- TruncatedSVD.py
+++ TruncatedSVD.py
@@ -1,17 +1,12 @@
-# -*- coding = utf-8 -*-
-# @Time : 2023/8/12 15:59
-# @File : TruncatedSVD.py
-# @Software : PyCharm.py
-
 import pickle
 from sklearn.decomposition import TruncatedSVD
 
-with open('reduced_embedding_sim.pkl', 'rb') as file:
+with open('./data/reduced_embedding_sim.pkl', 'rb') as file:
     data = pickle.load(file)
 
 matrix = list(data.values())
 
-n = 64
+n = 64  # dimension
 
 svd = TruncatedSVD(n_components=n)
 
@@ -19,6 +14,6 @@ matrix_n_d = svd.fit_transform(matrix)
 
 reduced_dict = {str(i): matrix_n_d[i] for i in range(len(matrix_n_d))}
 
-with open('data/reduced_dict.pkl', 'wb') as file:
+with open('./data/reduced_dict.pkl', 'wb') as file:
     pickle.dump(reduced_dict, file)
 
